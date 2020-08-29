010134c921db0925bce02e8c44679a36fdeb061e
hp <hp@DESKTOP-B64QDB1>
2020-08-29 12:46:15 -0400

    new

---
+++ Maven/src/test/java/demoMaven/Maven/AppiumTest.java
@@ -0,0 +1,18 @@
+package demoMaven.Maven;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+  
+	
+	@Test
+	public void postjira() {
+		System.out.println("postjira");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+	
+}
---
+++ Maven/src/test/java/demoMaven/Maven/Seleniumtest.java
@@ -0,0 +1,19 @@
+package demoMaven.Maven;
+
+import org.testng.annotations.Test;
+
+public class Seleniumtest {
+
+
+	@Test
+	public void Uiautomation() {
+		System.out.println("Uiautomation");
+	}
+	
+	@Test
+	public void elementsUi() {
+		System.out.println("elementsUi");
+	}
+	
+	
+}
